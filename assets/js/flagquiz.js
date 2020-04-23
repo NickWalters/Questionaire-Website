e75f23be2b0830b4dfd159c24bb70c2f7840762a
turnn <nick@elecnett.com.au>
2020-04-23 11:26:49 +0800

    Flag quiz SPA POC

---
+++ assets/js/flagquiz.js
@@ -0,0 +1,19 @@
+//Initiate when ready
+$(document).ready(function(){
+    console.log("Question number: 1");
+    setPageData(1);
+});
+
+$(document).on("click", ".button", function(){
+    question = parseInt($("#ajaxInput").html())+1;
+    console.log("Question number: "+question);
+    setPageData(question);
+});
+
+//Perform AJAX request
+function setPageData(question){
+    console.log(question)
+    $("#ajaxInput").html(question);
+    $("h4.questionHint").html("");
+    $("#flag").attr("src","images/flag0"+question+".svg");
+}
