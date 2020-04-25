f97cd29983a79d4aa7c18df0f968494025440076
Nick Walters <nicho.walters@gmail.com>
2020-04-25 21:15:25 +0800

    Added User Login (Not Finished)
    Followed Chapter of Miguel G, Flask Tutorial

--- routes.py
+++ routes.py
@@ -1,6 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect(url_for('index'))
+	return render_template('login.html', title='Sign In', form=form)
+	
 @app.route('/')
 def home():
 	return render_template('index.html')
