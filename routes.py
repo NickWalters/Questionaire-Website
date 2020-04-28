1650ed8884dd6e65471c3980e525d6aafe36afce
Nick Walters <nicho.walters@gmail.com>
2020-04-29 01:12:16 +0800

    Update routes.py

--- routes.py
+++ routes.py
@@ -11,14 +11,19 @@ def login():
 	if form.validate_on_submit():
 		flash('Login requested for user {}, remember_me={}'.format(
 			form.username.data, form.remember_me.data))
-		return redirect(url_for('login'))
+		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
 	logout_user()
 	flash("Logged Out")
-	return redirect(url_for('login'))
+	return redirect(url_for('index'))
+	
+
+@app.route('/index')
+def index():
+	return render_template('index.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
