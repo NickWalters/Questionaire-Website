9d913f76eca3620bf57fce581cd6c72b518e93d3
Nick Walters <nicho.walters@gmail.com>
2020-04-28 21:55:48 +0800

    Now able to register users

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, url_for, request, abort
+from app import app, db
+from werkzeug.urls import url_parse
 from forms import LoginForm, RegistrationForm
 from models import *
+from flask_login import login_user, logout_user, current_user, login_required
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
