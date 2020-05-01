e64ca13e4a19b2a7b58681e6a9740e8a4333b72e
Nick Walters <nicho.walters@gmail.com>
2020-05-01 20:29:15 +0800

    Login

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.static_folder = 'static'
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 import routes, models
 
--- routes.py
+++ routes.py
@@ -7,17 +7,32 @@ from flask_login import login_user, logout_user, current_user, login_required, L
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		if User.is_admin(current_user):
+			return redirect(url_for('hoster'))
+		else:
+			return redirect(url_for('index'))
+	
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(
-			form.username.data, form.remember_me.data))
-		return redirect(url_for('index'))
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if User.is_admin(current_user):
+			return redirect(url_for('hoster'))
+		if not next_page or url_parse(next_page).netloc !='':
+			next_page = url_for('index')
+		return redirect(next_page)
 	return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
+@login_required
 def logout():
 	logout_user()
-	flash("Logged Out")
+	flash("You have Logged Out")
 	return redirect(url_for('index'))
 	
 @app.route('/index')
