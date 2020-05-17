e98a55090244972034462cb22b06b500a78d9ab8
Rio A <riosyahakbar36@gmail.com>
2020-05-17 19:27:43 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+from app import app
+import unittest
+from app.models import User, Quiz, QuizContent, QuizStyle, Question, QuestionContent, QuestionChoice, UserAnswer
+from app.forms import LoginForm, RegistrationForm, StyleOneForm, StyleTwoForm
+import os
+from config import basedir
+
+
+class TestCase()
