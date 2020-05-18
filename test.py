52c75dfe5a2bbced9ddd16cdd2956318000866c0
Rio A <riosyahakbar36@gmail.com>
2020-05-19 05:41:27 +0800

    update test.py

--- test.py
+++ test.py
@@ -30,29 +30,68 @@ class TestCase(unittest.TestCase):
 #Registration Functionality Tests
     #tests a valid registration attempt
     def test_valid_registration(self):
-        response = self.register("user", "user@email.com", "password", "password")
+        response = self.register("user", "user@email.com", "password", "password", "submit")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Congratulations, you are now a registered user!', response.data)
 
     #Tests an invalid registration attempt where passwords do not match
     def test_invalid_registration_passwords(self):
-        response = self.register("user", "grae@email.com", "password", "invalid password")
+        response = self.register("user", "grae@email.com", "password", "invalid password", "submit")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Field must be equal to password', response.data)
 
     #Tests an invalid registration attempt where email is invalid
     def test_invalid_registration_email(self):
         #missing @ symbol
-        response = self.register("user", "invalid email.com", "password", "password")
+        response = self.register("user", "invalid email.com", "password", "password", "submit")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid email address', response.data)
 
         #missing domain
-        response = self.register("user", "invalid@email", "password", "password")
+        response = self.register("user", "invalid@email", "password", "password", "submit")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid email address', response.data)
 
         #invalid space inserted
-        response = self.register("user", "invalid@ email.com", "password", "password")
+        response = self.register("user", "invalid@ email.com", "password", "password", "submit")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid email address', response.data)
+
+    #Tests an invalid registration where the username and/or email are already taken
+    def test_invalid_registration_duplicates(self):
+        u1 = User(username = "existing_user", email = "existing_user@email.com")
+        db.session.add(u1)
+        db.session.commit()
+
+        #duplicate username
+        response = self.register("existing_user", "user@email.com", "password", "password", "submit")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'User name has been used', response.data)
+
+        #duplicate email
+        response = self.register("user", "existing_user@email.com", "password", "password", "submit")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Email has been used', response.data)
+    
+    #Tests an invalid registration where one of the fields has been left blank
+    def test_invalid_registration_missing(self):
+        response = self.register("", "user@email.com", "password", "password", "submit")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<title>Register</title>', response.data) #redirected to register page
+        self.assertIn(b'value=""', response.data) #at least 1 field has been left empty
+
+        response = self.register("user", "", "password", "password", "submit")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<title>Register</title>', response.data)
+        self.assertIn(b'value=""', response.data)
+
+        response = self.register("user", "user@email.com", "", "password", "submit")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<title>Register</title>', response.data)
+        self.assertIn(b'value=""', response.data)
+
+        response = self.register("user", "user@email.com", "password", "", "submit")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<title>Register</title>', response.data)
+        self.assertIn(b'value=""', response.data)
+
