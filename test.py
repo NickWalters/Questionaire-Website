93405fd691bf94077b7e57ee10394c28e4be95bd
Rio A <riosyahakbar36@gmail.com>
2020-05-18 15:14:44 +0800

    saving negligible changes

--- test.py
+++ test.py
@@ -6,4 +6,10 @@ import os
 from config import basedir
 
 
-class TestCase()
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
