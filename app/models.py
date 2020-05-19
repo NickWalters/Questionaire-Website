54e512ce8f55b431bfbaf9bef5c6c93f88d5b467
SeanChuu <62870306+SeanChuu@users.noreply.github.com>
2020-05-19 21:13:32 +0800

    reset

--- app/models.py
+++ app/models.py
@@ -17,6 +17,7 @@ class User(UserMixin, db.Model):
 
 	userAnswers = db.relationship('UserAnswer', backref='user', lazy="dynamic")
 	quizzes = db.relationship('Quiz', back_populates="creator")
+	attempt = db.relationship('User_attempt', backref='author', lazy=True)
 	
 	def is_admin(self):
 		return self.admin
@@ -40,7 +41,20 @@ class User(UserMixin, db.Model):
 		return lastanswer
 
 	def __repr__(self):
-		return self.username
+		return 'User: {}'.format(self.username)+' (admin:{}'.format(self.admin)+')'
+		
+class User_attempt(db.Model):
+	__tablename__ ='userattempt'
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+	quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+	totalscore = db.Column(db.Integer)
+	attemptnum = db.Column(db.Integer)
+	prevattempt = db.Column(db.Integer)
+	def __repr__(self):
+		return '<id:{}'.format(self.id)+' user_id:{}'.format(self.user_id)+ 'total:{}'.format(self.totalscore) + 'attemptnum:{}'.format(self.attemptnum) +'quiz id:{}'.format(self.quiz_id)
+
+
 
 class Quiz(db.Model):
 	__tablename__ = 'quiz'
@@ -68,7 +82,7 @@ class Quiz(db.Model):
 		return self.questions.filter_by(question_number = question_number).first()
 
 	def __repr__(self):
-		return self.quizname
+		return '<id:{}'.format(self.id) +'<quiz name:{}'.format(self.quizname) 
 
 class QuizContent(db.Model):
 	__tablename__ = 'quizContent'
@@ -112,8 +126,7 @@ class Question(db.Model):
 		return choices
 	
 	def __repr__(self):
-		return str(self.id)
-
+		return '< id:{}'.format(self.id)+' quiz_id:{}'.format(self.quiz_id) +' question num:{}'.format(self.question_number)
 class QuestionContent(db.Model):
 	__tablename__ = 'questionContent'
 	id = db.Column(db.Integer,primary_key=True)
@@ -135,7 +148,7 @@ class QuestionChoice(db.Model):
 	#choice_chosen = db.relationship('UserAnswer', backref='questionChoice', lazy=True)
 	
 	def __repr__(self):
-		return str(self.id)
+		return ' id:{}'.format(self.id) +'<question_id:{}'.format(self.question_id) +'<choice num:{}'.format(self.choice_number) + '< correct :{}'.format(self.choice_correct) + '<choice content:{}'.format(self.choice_content)  
 
 class UserAnswer(db.Model):
 	__tablename__ = 'userAnswer'
@@ -147,7 +160,7 @@ class UserAnswer(db.Model):
 	
 	answered_question = db.relationship('Question', back_populates="user_answers")
 	def __repr__(self):
-		return str(self.id)
+		return '<Answer id:{}'.format(self.id)+' user_id:{}'.format(self.user_id)+' question_id:{}'.format(self.question_id)+' choice_id_id:{}'.format(self.choice_id)+'>'
 
 @login.user_loader
 def load_user(id):
@@ -155,10 +168,179 @@ def load_user(id):
 
 #Create DB models
 db.create_all()
+#db.drop_all()
+#db.session.query(UserAnswer).delete()
+#db.session.query(User_attempt).delete()
+#db.session.add(User_attempt(totalscore = 1, attemptnum = 1, prevattempt = 1, user_id = 1, quiz_id = 1))
+#db.session.commit()
+
+db.session.add(User(username="admin", email="admin@admin.admin", admin=True, password_hash=generate_password_hash("admin")))
+db.session.add(User(username="user", email="user@user.user", admin=False, password_hash=generate_password_hash("user")))
+
+db.session.add(Quiz(quizname="Flag Quiz",creator_id=1,style=1))
+
+db.session.add(QuizStyle(style_name="Old flag style",template_file="quizStyle1.html"))
+
+db.session.add(QuizContent(quiz_id=1,text_content="Are you truly aware of the outside world? Do you have what it takes to test your knowledge on the flags of the world? Take our test !",img_content="au.svg"))
+
+db.session.add(Question(quiz_id=1,question_number=1))
+db.session.add(Question(quiz_id=1,question_number=2))
+db.session.add(Question(quiz_id=1,question_number=3))
+db.session.add(Question(quiz_id=1,question_number=4))
+db.session.add(Question(quiz_id=1,question_number=5))
+db.session.add(Question(quiz_id=1,question_number=6))
+db.session.add(Question(quiz_id=1,question_number=7))
+db.session.add(Question(quiz_id=1,question_number=8))
+db.session.add(Question(quiz_id=1,question_number=9))
+db.session.add(Question(quiz_id=1,question_number=10))
+
+db.session.add(QuestionContent(question_id=1,text_content="What country flag is this?",img_content="flag01.svg"))
+db.session.add(QuestionContent(question_id=2,text_content="What country flag is this?",img_content="flag02.svg"))
+db.session.add(QuestionContent(question_id=3,text_content="What country flag is this?",img_content="flag03.svg"))
+db.session.add(QuestionContent(question_id=4,text_content="What country flag is this?",img_content="flag04.svg"))
+db.session.add(QuestionContent(question_id=5,text_content="What country flag is this?",img_content="flag05.svg"))
+db.session.add(QuestionContent(question_id=6,text_content="What country flag is this?",img_content="flag06.svg"))
+db.session.add(QuestionContent(question_id=7,text_content="What country flag is this?",img_content="flag07.svg"))
+db.session.add(QuestionContent(question_id=8,text_content="What country flag is this?",img_content="flag08.svg"))
+db.session.add(QuestionContent(question_id=9,text_content="What country flag is this?",img_content="flag09.svg"))
+db.session.add(QuestionContent(question_id=10,text_content="What country flag is this?",img_content="flag10.svg"))
+
+#Question choices for flag quiz
+db.session.add(QuestionChoice(question_id=1,choice_number=1,choice_content="Namibia",choice_correct=False))
+db.session.add(QuestionChoice(question_id=1,choice_number=2,choice_content="Turks and Caicos Islands",choice_correct=False))
+db.session.add(QuestionChoice(question_id=1,choice_number=3,choice_content="Mongolia",choice_correct=False))
+db.session.add(QuestionChoice(question_id=1,choice_number=4,choice_content="Saint Pierre and Miquelon",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=2,choice_number=1,choice_content="French Polynesia",choice_correct=False))
+db.session.add(QuestionChoice(question_id=2,choice_number=2,choice_content="Maldives",choice_correct=False))
+db.session.add(QuestionChoice(question_id=2,choice_number=3,choice_content="Djibouti",choice_correct=False))
+db.session.add(QuestionChoice(question_id=2,choice_number=4,choice_content="Botswana",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=3,choice_number=1,choice_content="Anguilla",choice_correct=False))
+db.session.add(QuestionChoice(question_id=3,choice_number=2,choice_content="Lesotho",choice_correct=False))
+db.session.add(QuestionChoice(question_id=3,choice_number=3,choice_content="Western Sahara",choice_correct=False))
+db.session.add(QuestionChoice(question_id=3,choice_number=4,choice_content="Gabon",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=4,choice_number=1,choice_content="Heard Island and McDonald Islands",choice_correct=False))
+db.session.add(QuestionChoice(question_id=4,choice_number=2,choice_content="American Samoa",choice_correct=False))
+db.session.add(QuestionChoice(question_id=4,choice_number=3,choice_content="Zimbabwe",choice_correct=False))
+db.session.add(QuestionChoice(question_id=4,choice_number=4,choice_content="Puerto Rico",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=5,choice_number=1,choice_content="Isle of Man",choice_correct=False))
+db.session.add(QuestionChoice(question_id=5,choice_number=2,choice_content="South Georgia and the South Sandwich Islands",choice_correct=False))
+db.session.add(QuestionChoice(question_id=5,choice_number=3,choice_content="Iran",choice_correct=False))
+db.session.add(QuestionChoice(question_id=5,choice_number=4,choice_content="Pitcairn",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=6,choice_number=1,choice_content="Korea (Republic of)",choice_correct=False))
+db.session.add(QuestionChoice(question_id=6,choice_number=2,choice_content="Cayman Islands",choice_correct=False))
+db.session.add(QuestionChoice(question_id=6,choice_number=3,choice_content="Myanmar",choice_correct=False))
+db.session.add(QuestionChoice(question_id=6,choice_number=4,choice_content="Kiribati",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=7,choice_number=1,choice_content="Slovenia",choice_correct=False))
+db.session.add(QuestionChoice(question_id=7,choice_number=2,choice_content="Brunei",choice_correct=False))
+db.session.add(QuestionChoice(question_id=7,choice_number=3,choice_content="Saint Martin (French part)",choice_correct=False))
+db.session.add(QuestionChoice(question_id=7,choice_number=4,choice_content="Suriname",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=8,choice_number=1,choice_content="Finland",choice_correct=False))
+db.session.add(QuestionChoice(question_id=8,choice_number=2,choice_content="Fiji",choice_correct=False))
+db.session.add(QuestionChoice(question_id=8,choice_number=3,choice_content="Bahamas",choice_correct=False))
+db.session.add(QuestionChoice(question_id=8,choice_number=4,choice_content="Colombia",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=9,choice_number=1,choice_content="Rwanda",choice_correct=False))
+db.session.add(QuestionChoice(question_id=9,choice_number=2,choice_content="Georgia",choice_correct=False))
+db.session.add(QuestionChoice(question_id=9,choice_number=3,choice_content="Palestine",choice_correct=False))
+db.session.add(QuestionChoice(question_id=9,choice_number=4,choice_content="Czechia",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=10,choice_number=1,choice_content="Afghanistan",choice_correct=False))
+db.session.add(QuestionChoice(question_id=10,choice_number=2,choice_content="Indonesia",choice_correct=False))
+db.session.add(QuestionChoice(question_id=10,choice_number=3,choice_content="Angola",choice_correct=False))
+db.session.add(QuestionChoice(question_id=10,choice_number=4,choice_content="Jersey",choice_correct=True))
+
+db.session.add(Quiz(quizname="Language Quiz",creator_id=1,style=2))
+
+db.session.add(QuizStyle(style_name="Language quiz style",template_file="quizStyle2.html"))
+
+db.session.add(QuizContent(quiz_id=2,text_content="How much do you know about world culture, information and languages?",img_content="people-banner.png"))
+
+db.session.add(Question(quiz_id=2,question_number=1))
+db.session.add(Question(quiz_id=2,question_number=2))
+db.session.add(Question(quiz_id=2,question_number=3))
+db.session.add(Question(quiz_id=2,question_number=4))
+db.session.add(Question(quiz_id=2,question_number=5))
+db.session.add(Question(quiz_id=2,question_number=6))
+db.session.add(Question(quiz_id=2,question_number=7))
+db.session.add(Question(quiz_id=2,question_number=8))
+db.session.add(Question(quiz_id=2,question_number=9))
+db.session.add(Question(quiz_id=2,question_number=10))
+
+db.session.add(QuestionContent(question_id=11,text_content="What is the Official Language of Taiwan",img_content="taiwan-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=12,text_content="What is the Official Language of Australia",img_content="australia-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=13,text_content="What is the Official Language of Norway",img_content="norway-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=14,text_content="What is the Official Language of Colombia",img_content="colombia-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=15,text_content="What is the Official Language of Pakistan",img_content="pakistan-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=16,text_content="What is the Official Language of Ukraine",img_content="ukraine-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=17,text_content="What is the Official Language of Malaysia",img_content="malaysia-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=18,text_content="What is the Official Language of Mexico",img_content="mexico-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=19,text_content="What is the Official Language of Iran",img_content="iran-languageQuiz.jpg"))
+db.session.add(QuestionContent(question_id=20,text_content="What is the Official Language of Indonesia",img_content="indonesia-languageQuiz.jpg"))
+
+db.session.add(QuestionChoice(question_id=11,choice_number=1,choice_content="Taiwanese",choice_correct=False))
+db.session.add(QuestionChoice(question_id=11,choice_number=2,choice_content="Japanese",choice_correct=False))
+db.session.add(QuestionChoice(question_id=11,choice_number=3,choice_content="Mandarin",choice_correct=True))
+db.session.add(QuestionChoice(question_id=11,choice_number=4,choice_content="Cantonese",choice_correct=False))
+
+db.session.add(QuestionChoice(question_id=12,choice_number=1,choice_content="English",choice_correct=True))
+db.session.add(QuestionChoice(question_id=12,choice_number=2,choice_content="German",choice_correct=False))
+db.session.add(QuestionChoice(question_id=12,choice_number=3,choice_content="Korean",choice_correct=False))
+db.session.add(QuestionChoice(question_id=12,choice_number=4,choice_content="Russian",choice_correct=False))
+
+db.session.add(QuestionChoice(question_id=13,choice_number=1,choice_content="German",choice_correct=False))
+db.session.add(QuestionChoice(question_id=13,choice_number=2,choice_content="Spanish",choice_correct=False))
+db.session.add(QuestionChoice(question_id=13,choice_number=3,choice_content="English",choice_correct=False))
+db.session.add(QuestionChoice(question_id=13,choice_number=4,choice_content="Norwegian",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=14,choice_number=1,choice_content="Spanish",choice_correct=True))
+db.session.add(QuestionChoice(question_id=14,choice_number=2,choice_content="Irish",choice_correct=False))
+db.session.add(QuestionChoice(question_id=14,choice_number=3,choice_content="Dutch",choice_correct=False))
+db.session.add(QuestionChoice(question_id=14,choice_number=4,choice_content="French",choice_correct=False))
+
+db.session.add(QuestionChoice(question_id=15,choice_number=1,choice_content="Perish",choice_correct=False))
+db.session.add(QuestionChoice(question_id=15,choice_number=2,choice_content="Hindi",choice_correct=False))
+db.session.add(QuestionChoice(question_id=15,choice_number=3,choice_content="Arabic",choice_correct=False))
+db.session.add(QuestionChoice(question_id=15,choice_number=4,choice_content="Urdu",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=16,choice_number=1,choice_content="Ukrainian",choice_correct=True))
+db.session.add(QuestionChoice(question_id=16,choice_number=2,choice_content="Russian",choice_correct=False))
+db.session.add(QuestionChoice(question_id=16,choice_number=3,choice_content="Greenlandic",choice_correct=False))
+db.session.add(QuestionChoice(question_id=16,choice_number=4,choice_content="Galician",choice_correct=False))
+
+db.session.add(QuestionChoice(question_id=17,choice_number=1,choice_content="Malaysian",choice_correct=False))
+db.session.add(QuestionChoice(question_id=17,choice_number=2,choice_content="Malayense",choice_correct=False))
+db.session.add(QuestionChoice(question_id=17,choice_number=3,choice_content="Mandarin",choice_correct=False))
+db.session.add(QuestionChoice(question_id=17,choice_number=4,choice_content="Malay",choice_correct=True))
+
+db.session.add(QuestionChoice(question_id=18,choice_number=1,choice_content="Spanish",choice_correct=True))
+db.session.add(QuestionChoice(question_id=18,choice_number=2,choice_content="Mexian",choice_correct=False))
+db.session.add(QuestionChoice(question_id=18,choice_number=3,choice_content="Portuguese",choice_correct=False))
+db.session.add(QuestionChoice(question_id=18,choice_number=4,choice_content="Welsh",choice_correct=False))
+
+db.session.add(QuestionChoice(question_id=19,choice_number=1,choice_content="Arabic",choice_correct=False))
+db.session.add(QuestionChoice(question_id=19,choice_number=2,choice_content="Hebrew",choice_correct=False))
+db.session.add(QuestionChoice(question_id=19,choice_number=3,choice_content="Persian",choice_correct=True))
+db.session.add(QuestionChoice(question_id=19,choice_number=4,choice_content="Hindi",choice_correct=False))
+
+db.session.add(QuestionChoice(question_id=20,choice_number=1,choice_content="Malay",choice_correct=False))
+db.session.add(QuestionChoice(question_id=20,choice_number=2,choice_content="Indonesian",choice_correct=True))
+db.session.add(QuestionChoice(question_id=20,choice_number=3,choice_content="Mandarin",choice_correct=False))
+db.session.add(QuestionChoice(question_id=20,choice_number=4,choice_content="Thai",choice_correct=False))
+db.session.commit()
+#Print all DB data
+print(User_attempt.query.all())
 
 #Print all DB data
 #print(User.query.all())
-#print(Quiz.query.all())
+
+print(Quiz.query.all())
 #print(QuizStyle.query.all())
 #print(Question.query.all())
 #print(QuestionContent.query.all())
