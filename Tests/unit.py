d7d0b6ebfd7a27ec277b9a86a1e56d4b469d13d5
Rio A <riosyahakbar36@gmail.com>
2020-05-21 18:42:58 +0800

    Update test.py

--- Tests/unit.py
+++ Tests/unit.py
@@ -1,8 +1,8 @@
-from app import app
+import os
 import unittest
+from app import app, db
 from app.models import User, Quiz, QuizContent, QuizStyle, Question, QuestionContent, QuestionChoice, UserAnswer
 from app.forms import LoginForm, RegistrationForm, StyleOneForm, StyleTwoForm
-import os
 from config import basedir
 
 class TestCase(unittest.TestCase):
@@ -11,15 +11,19 @@ class TestCase(unittest.TestCase):
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
         self.app = app.test_client()
+        self.app_context = app.app_context()
+        self.app_context.push()
         db.create_all()
+        db.session.commit()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     #Helper Functions
 
-    def register(self, username, email, password, password2, submit):
+    def register(self, username, email, password, password2):
         return self.app.post("/register", data=dict(username=username, 
         email=email,
         password=password,
@@ -34,30 +38,30 @@ class TestCase(unittest.TestCase):
 #Registration Functionality Tests
     #tests a valid registration attempt
     def test_valid_registration(self):
-        response = self.register("user", "user@email.com", "password", "password", "submit")
+        response = self.register("user", "user@email.com", "password", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Congratulations, you are now a registered user!', response.data)
 
     #Tests an invalid registration attempt where passwords do not match
     def test_invalid_registration_passwords(self):
-        response = self.register("user", "grae@email.com", "password", "invalid password", "submit")
+        response = self.register("user", "grae@email.com", "password", "invalid password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Field must be equal to password', response.data)
 
     #Tests an invalid registration attempt where email is invalid
     def test_invalid_registration_email(self):
         #missing @ symbol
-        response = self.register("user", "invalid email.com", "password", "password", "submit")
+        response = self.register("user", "invalid email.com", "password", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid email address', response.data)
 
         #missing domain
-        response = self.register("user", "invalid@email", "password", "password", "submit")
+        response = self.register("user", "invalid@email", "password", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid email address', response.data)
 
         #invalid space inserted
-        response = self.register("user", "invalid@ email.com", "password", "password", "submit")
+        response = self.register("user", "invalid@ email.com", "password", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid email address', response.data)
 
@@ -68,33 +72,33 @@ class TestCase(unittest.TestCase):
         db.session.commit()
 
         #duplicate username
-        response = self.register("existing_user", "user@email.com", "password", "password", "submit")
+        response = self.register("existing_user", "user@email.com", "password", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'User name has been used', response.data)
 
         #duplicate email
-        response = self.register("user", "existing_user@email.com", "password", "password", "submit")
+        response = self.register("user", "existing_user@email.com", "password", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Email has been used', response.data)
     
     #Tests an invalid registration where one of the fields has been left blank
     def test_invalid_registration_missing(self):
-        response = self.register("", "user@email.com", "password", "password", "submit")
+        response = self.register("", "user@email.com", "password", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<title>Register</title>', response.data) #redirected to register page
         self.assertIn(b'value=""', response.data) #at least 1 field has been left empty
 
-        response = self.register("user", "", "password", "password", "submit")
+        response = self.register("user", "", "password", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<title>Register</title>', response.data)
         self.assertIn(b'value=""', response.data)
 
-        response = self.register("user", "user@email.com", "", "password", "submit")
+        response = self.register("user", "user@email.com", "", "password")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<title>Register</title>', response.data)
         self.assertIn(b'value=""', response.data)
 
-        response = self.register("user", "user@email.com", "password", "", "submit")
+        response = self.register("user", "user@email.com", "password", "")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<title>Register</title>', response.data)
         self.assertIn(b'value=""', response.data)
@@ -138,7 +142,7 @@ class TestCase(unittest.TestCase):
     #test logout
     def test_logout(self):
         self.app.get('/register', follow_redirects = True)
-        self.register("user", "user@email.com", "password", "password", "submit")
+        self.register("user", "user@email.com", "password", "password")
         self.app.get('/login', follow_redirects = True)
         self.login("user", "password")
         response = self.app.get('/logout', follow_redirects = True)
@@ -166,3 +170,6 @@ class TestCase(unittest.TestCase):
 
         response = self.login("john", "password")
         self.assertNotIn(b'Welcome Admin John!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
